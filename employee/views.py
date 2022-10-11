4dc31c63399880d1024228f37e295ccff3a80083
VitanovGeorgi <vitanovgeorgi.gv@gmail.com>
2022-10-11 12:53:28 +0200

    serializers created

--- employee/views.py
+++ employee/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from rest_framework import status, serializers
+from rest_framework.response import Response
+from rest_framework.decorators import api_view, renderer_classes
+from rest_framework_swagger import renderers
+from .models import Employee, Team, PartialTeamEmployeeRelation as TERelation
+# from .serializers import EmployeeSerializer, TeamSerializer, PartialTeamEployeeSerializer
 
 # Create your views here.
